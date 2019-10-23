f4993c5657d4f614e335a853d2852cebccd2ac5b
anushkadewoolkar <dewoolkar6949@students.d211.org>
2019-10-23 17:48:51 -0500

    Updated flashcards

--- app/src/main/java/com/example/harrylum/cardistry/MainActivity.java
+++ app/src/main/java/com/example/harrylum/cardistry/MainActivity.java
@@ -13,6 +13,8 @@ import android.widget.TextView;
 import java.util.List;
 import java.util.Random;
 
+//
+
 public class MainActivity extends AppCompatActivity {
 
     FlashcardDatabase flashcardDatabase;
